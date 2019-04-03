69dcf34556436a1099cf98e57b3a617239e9ccf2
konstantinos <Konstantinos.Mantzoukas.1@city.ac.uk>
2019-04-03 18:17:20 +0300

    Initial commit of the loadandanonymize atomic service as a Spring Boot application

---
+++ services/loadandanonymize/src/main/java/uk/ac/city/SparkSubmitServiceConfiguration.java
@@ -0,0 +1,47 @@
+package uk.ac.city;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spark")
+public class SparkSubmitServiceConfiguration {
+
+    private String appName = "LoadAndAnonymizeService";
+
+    private String appClass = "uk.ac.city.services.AnonymizeData";
+
+    private String appJar = "/home/abfc149/anonymizedata_2.11-0.1-SNAPSHOT.jar";
+
+    private String master = "spark://10.207.1.102:7077";
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppClass() {
+        return appClass;
+    }
+
+    public void setAppClass(String appClass) {
+        this.appClass = appClass;
+    }
+
+    public String getAppJar() {
+        return appJar;
+    }
+
+    public void setAppJar(String appJar) {
+        this.appJar = appJar;
+    }
+
+    public String getMaster() {
+        return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
+}
