bbded0d0446e32dadba0c68a7a3bb1616b788ad1
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2019-06-19 11:27:05 +0100

    Added scripts for noe event captor service execution

---
+++ evaluation/no-event-captors/no-event-captors-anonymizedata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in {1..1000}
+do
+spark-submit \
+--master spark://10.207.1.102:7077 \
+--class uk.ac.city.services.AnonymizeData \
+--deploy-mode client \
+--conf "spark.driver.extraJavaOptions=-javaagent:/home/abfc149/toreador-demo/captors/MetricCaptor.jar=emitter=socket,host=10.207.1.103,port=10334,eventStype=NONE" \
+--conf "spark.executor.extraJavaOptions=-javaagent:/home/abfc149/toreador-demo/captors/MetricCaptor.jar=emitter=socket,host=10.207.1.103,port=10334,eventStype=NONE" \
+/home/abfc149/anonymizedata_2.11-0.1-SNAPSHOT.jar
+done
